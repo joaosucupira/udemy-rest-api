4a1b2f17372733447708e3c216c9c3b67fdc951a
João Sucupira <113265060+joaosucupira@users.noreply.github.com>
2024-02-14 10:52:02 -0300

    Update 3
    Finalizado o módulo 6; Implementação das funcionalidades do SQLalchemy; Criação dos modelos de Item e Store; Implementação dos métodos de put e delete assim como os atributos de commit para o banco de dados que estamos usando.

--- app.py
+++ app.py
@@ -2,27 +2,44 @@
 # from flask import Flask, request
 # from flask_smorest import abort
 # from db import items, stores
+import os
 from flask import Flask
 from flask_smorest import Api
+
+from db import db
+import models
 
 from resources.items import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-api = Api(app)
+    api = Api(app)
+    
+    with app.app_context():
+        db.create_all()
+    
+    # type: ignore
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    
+    return app
 
 # @app.post("/store")
 # def create_store():
--- resources/items.py
+++ resources/items.py
@@ -2,8 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
+from sqlalchemy.exc import SQLAlchemyError
 
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations with items")
@@ -12,50 +14,45 @@ blp = Blueprint("Items", __name__, description="Operations with items")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message='Item not found.')
+        item = ItemModel.query.get_or_404(item_id)
+        return item
     
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message":"Item deleted."}
-        except KeyError:
-            abort(404, message='item not found')
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."} #, 200
             
     @blp.arguments(ItemUpdateSchema) 
     @blp.response(200, ItemSchema)
     def put(self,item_data, item_id):
-
-        try:
-            item = items[item_id]
-            item |= item_data
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_data, **item_data)
+            
+        db.session.add(item)
+        db.session.commit()
         
-            return item
-    
-        except KeyError:
-            abort(404, message="item not found.")
-
+        return item    
+        
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
     
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data): 
-        for item in items.values():
-            if(
-            item_data["name"] == item_data["name"]
-            and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-            
-    
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
-    
-        return item, 201
+        item = ItemModel(**item_data)
+        
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+        
+        return item#,  201
